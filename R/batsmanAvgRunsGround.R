3aa55dc214ca5134b772800827a2e6fcd939d7a1
Tinniam V Ganesh <tvganesh.85@gmail.com>
2015-07-10 16:23:02 +0530

    Added AvgRuns,AvgWkts at Ground

---
+++ R/batsmanAvgRunsGround.R
@@ -0,0 +1,25 @@
+##########################################################################################
+# Designed and developed by Tinniam V Ganesh
+# Date : 10 Jul 2015
+# Function: batsmanAvgRunsGround
+# This function plots the average runs scored by batsman at the ground. The xlabels indicate
+# the number of innings at ground
+#
+###########################################################################################
+
+batsmanAvgRunsGround <- function(file, name="A Latecut"){
+    
+    
+    batsman <- clean(file)
+    meanRuns <- batsman %>% group_by(Ground) %>% summarise(m= mean(Runs))
+    countInnings <- batsman %>% group_by(Ground) %>% summarise(len=length(Runs))
+    par(mar=c(9,4,3,2))
+    ground <- as.vector(meanRuns$Ground)
+    values <- paste(ground,"-",countInnings$len)
+    atitle <- paste(name,"'s Average Runs at Ground")
+    barplot(meanRuns$m,names=values,las=2,ylab="Average Runs", 
+            col=rainbow(length(meanRuns$m)),main=atitle)
+    
+    mtext("Ground - No of innings", side=1, line=7.5, adj=1.0, cex=1.0, col="black")
+    mtext("Data source-Courtesy:ESPN Cricinfo", side=3, line=0, adj=1.0, cex=0.8, col="blue")
+}
---
+++ R/bowlerAvgWktsGround.R
@@ -0,0 +1,26 @@
+##########################################################################################
+# Designed and developed by Tinniam V Ganesh
+# Date : 10 Jul 2015
+# Function: bowlerAvgWktsGround
+# This function plots the average runs scored by batsman at the ground. The xlabels indicate
+# the number of innings at ground
+#
+###########################################################################################
+
+bowlerAvgWktsGround <- function(file, name="A Chinaman"){
+    
+    
+    bowler <- cleanBowlerData(file)
+    meanWkts <- bowler %>% group_by(Ground) %>% summarise(m= mean(Wkts))
+
+    countInnings <- bowler %>% group_by(Ground) %>% summarise(len=length(Wkts))
+    par(mar=c(9,4,3,2))
+    ground <- as.vector(meanWkts$Ground)
+    values <- paste(ground,"-",countInnings$len)
+    atitle <- paste(name,"'s Average Wickets at Ground")
+    barplot(meanWkts$m,names=values,las=2,ylab="Average Wickets", 
+            col=rainbow(length(meanWkts$m)),main=atitle)
+    
+    mtext("Ground (No of innings)", side=1, line=7.5, adj=1.0, cex=1.0, col="black")
+    mtext("Data source-Courtesy:ESPN Cricinfo", side=3, line=0, adj=1.0, cex=0.8, col="blue")
+}
